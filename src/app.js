8e7f4bf607c918e6574ab9750afec84042c0d9b2
ridhi-tuteja <ridhituteja@gmail.com>
2025-06-27 15:44:44 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -14,4 +14,9 @@ app.use(express.urlencoded({extended:true,limit:"16kb"}));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+//routes import
+import userRouter from './routes/user.route.js';
+//route declaration
+app.use('/api/v1/users', userRouter);    //get is not used here as all the files are seperated into different files(not at a single place)
+
 export {app};
